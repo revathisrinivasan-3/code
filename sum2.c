26ee72196eb7fb3b1fa43e358db4ae2c32be712e
revathisrinivasan-3 <30317958+revathisrinivasan-3@users.noreply.github.com>
2017-07-24 12:22:10 +0530

    Create sum2.c

---
+++ sum2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ int i,sum=0,n;
+ printf("Enter the numbers: ");
+ scanf("%d",&n);
+ for(i=1;i<=n;i++)
+ {
+  sum=+i;
+  }
+  printf("%d sum is ");
+  return 0;
+  }
