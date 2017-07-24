dc80335868f0cb713b5310d693c2bdcd8e78a75d
revathisrinivasan-3 <30317958+revathisrinivasan-3@users.noreply.github.com>
2017-07-24 14:15:51 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,31 @@
+#include <stdio.h> 
+#define MAX_SIZE 100 //Maximum size of the string
+int main()
+{
+    char string[MAX_SIZE];
+    int alphabets, digits,alphanumeric,i;
+    alphabets = digits = alphanumeric = i = 0;
+    printf("Enter any string : ");
+    gets(string);
+    while(string[i]!='\0')
+    {
+        if((string[i]>='a' && string[i]<='z') || (string[i]>='A' && string[i]<='Z'))
+        {
+            alphabets++;
+        }
+        else if(string[i]>='0' && string[i]<='9')
+        {
+            digits++;
+        }
+        else
+        {
+            alphanumeric++;
+        }
+ 
+        i++;
+    }
+    printf("Alphabets = %d\n", alphabets);
+    printf("Digits = %d\n", digits);
+    printf("Special characters = %d\n", alphanumeric);
+    return 0;
+}
